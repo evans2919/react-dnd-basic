27c6c095ff5c86ec932e073002ba715f5cfe316f
evans2919 <evansalcantara29@outlook.com>
2023-07-14 10:32:57 +0200

    Añadiendo estado inicial de listado, librería DnD, utilización de librería, modificación de Array según el arrastre

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,75 @@
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+
+const initialToDos = [
+    {
+        id: 1,
+        title: "Pokemon Rubi",
+    },
+
+    {
+        id: 2,
+        title: "Pokemon Diamante",
+    },
+    {
+        id: 3,
+        title: "Pokemon Perla",
+    },
+    {
+        id: 4,
+        title: "Pokemon Cristal",
+    },
+];
 const App = () => {
-  return (
-    "buenas tardes"
-  );
-}
+    const [toDos, setToDos] = useState(initialToDos);
+
+    const handleDragEnd = (result) => {
+        if (!result.destination) return;
+        const dropStart = result.source.index;
+        const dropEnd = result.destination.index;
+        const copyArray = [...toDos];
+        const [reorderedItems] = copyArray.splice(dropStart, 1);
+        copyArray.splice(dropEnd, 0, reorderedItems);
+        setToDos(copyArray);
+    };    
+
+    return (
+        <>
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <h1>Pokemon Drag and Drop</h1>
+                <Droppable droppableId="todos">
+                    {(droppableProvider) => (
+                        <ul
+                            ref={droppableProvider.innerRef}
+                            {...droppableProvider.droppableProps}
+                        >
+                            {toDos.map((toDo, index) => (
+                                <Draggable
+                                    draggableId={`${toDo.id}`}
+                                    key={toDo.id}
+                                    index={index}
+                                >
+                                    {(draggableProvider) => (
+                                        <li
+                                            ref={draggableProvider.innerRef}
+                                            {...draggableProvider.dragHandleProps}
+                                            {...draggableProvider.draggableProps}
+                                        >
+                                            <br></br>
+                                            {toDo.title}
+                                            <br></br>
+                                            <br></br>
+                                        </li>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {droppableProvider.placeholder}
+                        </ul>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </>
+    );
+};
 
 export default App;
